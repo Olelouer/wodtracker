95af5a89ae484ffdee1a9017ed55fd56d1d9b404
Olelouer <olivier.lelouer@protonmail.com>
2026-01-28 20:46:53 +0100

    add clerk config

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import WodListCpn from "@/components/list/WodListCpn";
 import { getWods } from '@/app/dashboard/actions';
+import { currentUser } from '@clerk/nextjs/server'
 
 const Dashboard = async () => {
     const wods = await getWods();
+    const user = await currentUser();
+    console.log(user);
     return (
         <div>
             <h1 className="title">Dashboard WodTracker</h1>
